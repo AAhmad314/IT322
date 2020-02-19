7655c3ada883e8d77e89c9553db8f0317c8d982c
AAhmad314 <aa2747@njit.edu>
2020-02-19 00:08:02 -0500

    First Commit, Styling and Landing Page

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+ <head>
+  <link rel="stylesheet" type="text/css" href="mystyle.css">
+  <meta name="viewport" content"width=device=width, initial-scale=1.0">
+  <script type="text/javascript">
+  </script>
+ </head>
+
+<body>
+<h1 class="center">Welcome To the Junkster Website</h1>
+<br>
+<h4 class="center">Sign In Below To Begin Your Journey</h4>
+
+<div class="form">
+   <form action="#" method="POST" >
+    <!--Textboxes and Placeholders-->
+	<h2 class="center"> Insert Login Information </h2>
+	<hr> <br> <br>
+     <input id="username" name="username" type="text" placeholder="Enter Username"/> 
+	<br>
+     <input id="password" name="password" type="password" placeholder="Enter Password"/>
+	<br>
+     <input type="submit" name="submit" value="Login" />
+  
+     <!-- Link to Registration Page --> 
+     <a class="link" href="registration.php">Click Here to Become a Member Today!</a>
+
+   </form>
+</div>	
+
+</body>
+</html>
+
